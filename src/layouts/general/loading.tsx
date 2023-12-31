2b7d71eaa11df215773750caaeebf0cac53ddb70
shepherrrd <shefkid2@gmail.com>
2023-12-31 17:32:56 +0100

    gg

--- src/layouts/general/loading.tsx
+++ src/layouts/general/loading.tsx
@@ -25,7 +25,7 @@ const LoadingGeneral: React.FC<LoadingGeneralProps> = ({
         }
         return newProgress;
       });
-    }, 500000);
+    }, 50);
 
     return () => {
       clearInterval(interval);
@@ -34,7 +34,8 @@ const LoadingGeneral: React.FC<LoadingGeneralProps> = ({
   return (
     <div
       id="TopMessageRoot"
-      className="bg-black md:flex flex-col gap-10 w-full md:h-[1024px] items-start pt-16 md:px-[148px]">
+      className="bg-black md:flex flex-col gap-10 w-full md:h-[1024px] items-start pt-16 md:px-[148px]"
+    >
       <Image
         src="/logo.png"
         alt="GDSCBabcockUniversityHorizontalWhite"
